1f5ddef78bb1210f9e0a3cdc0102239298d7b628
Erik Fabrizzi <erik.fabrizzi@fau.de>
2024-11-12 03:22:25 +0100

    Proto multiblock

--- include/path_merge.cuh
+++ include/path_merge.cuh
@@ -97,3 +97,102 @@ __global__ void mergeSmall_k(const T *v_1_ptr,
         }
     }
 }
+
+template <class T>
+__global__ void mergeSmall_k2(const T *v_1_ptr,
+                              const size_t v_1_size,
+                              const T *v_2_ptr,
+                              const size_t v_2_size,
+                              T *v_out_ptr,
+                              const size_t v_out_size)
+{
+    int thread_idx = threadIdx.x + blockIdx.x * blockDim.x;
+    coordinate K = {0, 0};
+    coordinate P = {0, 0};
+    coordinate Q = {0, 0};
+    if (thread_idx > v_1_size)
+    {
+        K.x = thread_idx - v_1_size;
+        K.y = v_1_size;
+        P.x = v_1_size;
+        P.y = thread_idx - v_1_size;
+    }
+    else
+    {
+        K.y = thread_idx;
+        P.x = thread_idx;
+    }
+    __shared__ int2 Q_shift;
+    //  Do Binary search in last thread of block
+    if (threadIdx.x == blockDim.x - 1)
+    {
+        Q_shift.x = 0;
+        Q_shift.y = 0;
+        while (true)
+        {
+            size_t offset = abs((K.y - P.y)) / 2;
+            Q_shift.x = K.x + offset;
+            Q_shift.y = K.y - offset;
+            if (Q_shift.y >= 0 && Q_shift.x <= v_2_size && (Q_shift.y == v_1_size || Q_shift.x == 0 || v_1_ptr[Q_shift.y] > v_2_ptr[Q_shift.x - 1]))
+            {
+                if (Q_shift.x == v_2_size || Q_shift.y == 0 || v_1_ptr[Q_shift.y - 1] <= v_2_ptr[Q_shift.x])
+                {
+                    break;
+                }
+                else
+                {
+                    K.x = Q_shift.x + 1;
+                    K.y = Q_shift.y - 1;
+                }
+            }
+            else
+            {
+                P.x = Q_shift.x - 1;
+                P.y = Q_shift.y + 1;
+            }
+        }
+    }
+    __syncthreads();
+    printf("Block %d Q_shift %d %d \n", blockIdx.x, Q_shift.x, Q_shift.y);
+    K = {Q_shift.x - (int)blockDim.x + (int)threadIdx.x +1, Q_shift.y};
+    P = {Q_shift.x , Q_shift.y - (int)blockDim.x + (int)threadIdx.x +1};
+    Q = {0, 0};
+    __shared__ T A[4];
+    __shared__ T B[4];
+    int base_idx_a = max(0,Q_shift.y - (int)blockDim.x + 1);
+    int base_idx_b = max(0,Q_shift.x - (int)blockDim.x + 1);
+    A[threadIdx.x] = v_1_ptr[base_idx_a + threadIdx.x];
+    B[threadIdx.x] = v_2_ptr[base_idx_b + threadIdx.x];
+    __syncthreads();
+    while (true && thread_idx < v_out_size)
+    {
+        size_t offset = abs((K.y - P.y)) / 2;
+        Q.x = K.x + offset;
+        Q.y = K.y - offset;
+        if (Q.y >= 0 && Q.x <= v_2_size && (Q.y == v_1_size || Q.x == 0 || A[Q.y-base_idx_a] > B[Q.x - base_idx_b -1]))
+        {
+            if (Q.x == v_2_size || Q.y == 0 || A[Q.y -base_idx_a - 1] <= B[Q.x -base_idx_b])
+            {
+                if (Q.y < v_1_size && (Q.x == v_2_size || A[Q.y-base_idx_a] <= B[Q.x-base_idx_b]))
+                {
+                    v_out_ptr[thread_idx] = A[Q.y-base_idx_a];
+                }
+                else
+                {
+                    v_out_ptr[thread_idx] = B[Q.x -base_idx_b];
+                }
+                break;
+            }
+            else
+            {
+                K.x = Q.x + 1;
+                K.y = Q.y - 1;
+            }
+        }
+        else
+        {
+            P.x = Q.x - 1;
+            P.y = Q.y + 1;
+        }
+    }
+}
--- src/main.cu
+++ src/main.cu
@@ -6,43 +6,44 @@
 #include <path_merge.cuh>
 #include <string>
 
-
 using v_type = int;
 
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
 
-   if(argc<3)
-   {
-      std::cout << "Usage: " << argv[0] << " <size-a> " << " <size-b> " <<std::endl; 
-      abort();
-   }
-   printGPUInfo();
+    if (argc < 3)
+    {
+        std::cout << "Usage: " << argv[0] << " <size-a> " << " <size-b> " << std::endl;
+        abort();
+    }
+    printGPUInfo();
 
-   std::vector<v_type> vector_1 = build_random_vector<v_type>(std::stoi(argv[1]), -100, 100);
-   std::vector<v_type> vector_2 = build_random_vector<v_type>(std::stoi(argv[2]), -100, 100);
-   std::vector<v_type> vector_out(vector_1.size() + vector_2.size());
-   v_type *v_1_gpu, *v_2_gpu, *v_out_gpu;
+    //    std::vector<v_type> vector_1 = build_random_vector<v_type>(std::stoi(argv[1]), -100, 100);
+    //    std::vector<v_type> vector_2 = build_random_vector<v_type>(std::stoi(argv[2]), -100, 100);
+    std::vector<v_type> vector_1 = {3, 6, 8, 17, 19, 20};
+    std::vector<v_type> vector_2 = {2, 5, 11, 12, 13, 21};
+    std::vector<v_type> vector_out(vector_1.size() + vector_2.size());
+    v_type *v_1_gpu, *v_2_gpu, *v_out_gpu;
 
-   std::sort(vector_1.begin(), vector_1.end());
-   std::sort(vector_2.begin(), vector_2.end());
+    std::sort(vector_1.begin(), vector_1.end());
+    std::sort(vector_2.begin(), vector_2.end());
 
-   cudaMalloc(&v_1_gpu  , vector_sizeof(vector_1));
-   cudaMalloc(&v_2_gpu  , vector_sizeof(vector_2));
-   cudaMalloc(&v_out_gpu, vector_sizeof(vector_out));
+    cudaMalloc(&v_1_gpu, vector_sizeof(vector_1));
+    cudaMalloc(&v_2_gpu, vector_sizeof(vector_2));
+    cudaMalloc(&v_out_gpu, vector_sizeof(vector_out));
 
-   cudaMemcpy(v_1_gpu, vector_1.data(), vector_sizeof(vector_1), cudaMemcpyHostToDevice);
-   cudaMemcpy(v_2_gpu, vector_2.data(), vector_sizeof(vector_2), cudaMemcpyHostToDevice);
+    cudaMemcpy(v_1_gpu, vector_1.data(), vector_sizeof(vector_1), cudaMemcpyHostToDevice);
+    cudaMemcpy(v_2_gpu, vector_2.data(), vector_sizeof(vector_2), cudaMemcpyHostToDevice);
 
-   mergeSmall_k<<<1, vector_out.size()>>>(v_1_gpu, vector_1.size(),
-                                              v_2_gpu, vector_2.size(),
-                                              v_out_gpu, vector_out.size());
+    mergeSmall_k2<<<3, 4>>>(v_1_gpu, vector_1.size(),
+                            v_2_gpu, vector_2.size(),
+                            v_out_gpu, vector_out.size());
 
-   cudaMemcpy(vector_out.data(), v_out_gpu, vector_sizeof(vector_out), cudaMemcpyDeviceToHost);
+    cudaMemcpy(vector_out.data(), v_out_gpu, vector_sizeof(vector_out), cudaMemcpyDeviceToHost);
 
-   print_vector(vector_1, "A = ");
-   print_vector(vector_2, "B = ");
-   auto merged = mergeSmall_k_cpu(vector_1, vector_2);
-   print_vector(merged, "CPU merge: ");
-   print_vector(vector_out, "GPU merge: ");
+    print_vector(vector_1, "A = ");
+    print_vector(vector_2, "B = ");
+    auto merged = mergeSmall_k_cpu(vector_1, vector_2);
+    print_vector(merged, "CPU merge: ");
+    print_vector(vector_out, "GPU merge: ");
 }
