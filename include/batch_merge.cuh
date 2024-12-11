abf02a7d5bdd66cfd816b77f65fad60c476c7472
Erik Fabrizzi <erik.fabrizzi@fau.de>
2024-12-11 15:44:27 +0100

    Benchmarking setup for batchMerge

--- include/batch_merge.cuh
+++ include/batch_merge.cuh
@@ -12,25 +12,25 @@ using std::numeric_limits;
 template <typename T>
 __global__ void mergeSmallBatch_k(T *batches, unsigned short *A_sizes, unsigned N, unsigned short d)
 {
-  //since MAX_BATCH_SIZE is 1024, we can use shared memory to store both input and output
+  // since MAX_BATCH_SIZE is 1024, we can use shared memory to store both input and output
   __shared__ T shared_in[MAX_BATCH_SIZE];
   __shared__ T shared_out[MAX_BATCH_SIZE];
 
-  //define indexes
+  // define indexes
   unsigned batch_per_block = blockDim.x / d;
-  unsigned batch_block_idx = threadIdx.x / d; //Qt 
-  unsigned thread_local_idx = threadIdx.x - batch_block_idx * d; //tidx
-  unsigned thread_global_idx = batch_block_idx + blockIdx.x * batch_per_block; //gbx
+  unsigned batch_block_idx = threadIdx.x / d;                                  // Qt
+  unsigned thread_local_idx = threadIdx.x - batch_block_idx * d;               // tidx
+  unsigned thread_global_idx = batch_block_idx + blockIdx.x * batch_per_block; // gbx
 
-  //load data into shared memory
-  if(thread_global_idx < N)
+  // load data into shared memory
+  if (thread_global_idx < N)
   {
     shared_in[threadIdx.x] = batches[blockDim.x * blockIdx.x + threadIdx.x];
   }
   __syncthreads();
 
-  //parallel merge
-  if(thread_global_idx < N)
+  // parallel merge
+  if (thread_global_idx < N)
   {
 
     T *batch = shared_in + batch_block_idx * d;
@@ -39,7 +39,7 @@ __global__ void mergeSmallBatch_k(T *batches, unsigned short *A_sizes, unsigned 
 
     T *A_local = batch;
     T *B_local = batch + local_A_size;
-    
+
     uint2 K, P, Q;
 
     K.x = thread_local_idx <= local_A_size ? 0 : thread_local_idx - local_A_size;
@@ -82,8 +82,8 @@ __global__ void mergeSmallBatch_k(T *batches, unsigned short *A_sizes, unsigned 
   }
   __syncthreads();
 
-  //write data back to global memory
-  if(thread_global_idx < N)
+  // write data back to global memory
+  if (thread_global_idx < N)
   {
     batches[blockDim.x * blockIdx.x + threadIdx.x] = shared_out[threadIdx.x];
   }
@@ -98,25 +98,24 @@ __global__ void mergeSmallBatch_k(T *batches, unsigned short *A_sizes, unsigned 
 template <class T>
 const std::vector<unsigned short> build_and_sort_batches(std::vector<T> &batches, unsigned N, unsigned short d, T min = numeric_limits<T>::min(), T max = numeric_limits<T>::max())
 {
-  std::cout << "Generating and sorting " << N << " random batches of size " << d << " ..." << std::endl;
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_real_distribution<double> dis(min, max);
-  std::generate(batches.begin(), batches.end(), [&dis, &gen]() { return static_cast<T>(dis(gen)); });
+  std::generate(batches.begin(), batches.end(), [&dis, &gen]()
+                { return static_cast<T>(dis(gen)); });
 
   std::uniform_int_distribution<unsigned short> splitter(1, d - 1);
   std::vector<unsigned short> offsets(N);
-  std::generate(offsets.begin(), offsets.end(), [&splitter, &gen]() { return splitter(gen); });
+  std::generate(offsets.begin(), offsets.end(), [&splitter, &gen]()
+                { return splitter(gen); });
 
-#pragma omp parallel for
-  for(unsigned batch_idx = 0; batch_idx < N; batch_idx++)
+  #pragma omp parallel for
+  for (unsigned batch_idx = 0; batch_idx < N; batch_idx++)
   {
     auto curr_batch_start = batches.begin() + batch_idx * d;
     std::sort(curr_batch_start, curr_batch_start + offsets[batch_idx]);
     std::sort(curr_batch_start + offsets[batch_idx], curr_batch_start + d);
   }
-  std::cout << "Batches sorted." << std::endl;
-
   return offsets;
 }
 
@@ -124,14 +123,14 @@ const std::vector<unsigned short> build_and_sort_batches(std::vector<T> &batches
  * @brief Merge N batches of size d (|A_i| + |B_i| = d) on the CPU.
  */
 template <class T>
-void merge_batch_cpu(std::vector<T> &batches, const std::vector<unsigned short> &offsets, unsigned N, unsigned short d)
+float merge_batch_cpu(std::vector<T> &batches, const std::vector<unsigned short> &offsets, unsigned N, unsigned short d)
 {
   auto start = std::chrono::high_resolution_clock::now();
-  for(unsigned batch_idx = 0; batch_idx < N; batch_idx++)
+  for (unsigned batch_idx = 0; batch_idx < N; batch_idx++)
   {
     auto curr_batch_start = batches.begin() + batch_idx * d;
     std::inplace_merge(curr_batch_start, curr_batch_start + offsets[batch_idx], curr_batch_start + d);
   }
   auto end = std::chrono::high_resolution_clock::now();
-  std::cout << "CPU merge time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+  return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 }
--- src/bench_batchMerge.cu
+++ src/bench_batchMerge.cu
@@ -6,19 +6,18 @@
 
 using v_type = int;
 
-
 int main(int argc, char **argv)
 {
 
   if (argc < 3)
   {
-      std::cout << "Usage: " << argv[0] << " <N> " << " <d> " << std::endl;
-      abort();
+    std::cout << "Usage: " << argv[0] << " <N> " << " <d> " << std::endl;
+    abort();
   }
 
   unsigned N = std::stoi(argv[1]);
   unsigned d_arg = std::stoi(argv[2]);
-  if(d_arg > MAX_BATCH_SIZE || d_arg < 2)
+  if (d_arg > MAX_BATCH_SIZE || d_arg < 2)
   {
     std::cout << "d must be between 2 and 1024" << std::endl;
     abort();
@@ -30,33 +29,43 @@ int main(int argc, char **argv)
 
   std::vector<v_type> vector_out(N * d);
   v_type *v_A_B_gpu;
+  v_type *v_A_B_buffer;
   unsigned short *v_A_sizes_gpu;
 
-  TIME_EVENT_DEFINE(timing_kernel);TIME_EVENT_CREATE(timing_kernel);
-  float timing_kernel_ms = 0.0f;
+  TIME_EVENT_DEFINE(timing_kernel);
+  TIME_EVENT_CREATE(timing_kernel);
+  float time_kernel_ms = 0.0f;
 
   cudaMalloc(&v_A_B_gpu, vector_sizeof(A_B_vectors));
+  cudaMalloc(&v_A_B_buffer, vector_sizeof(A_B_vectors));
+
   cudaMalloc(&v_A_sizes_gpu, vector_sizeof(A_sizes));
   cudaMemcpy(v_A_B_gpu, A_B_vectors.data(), vector_sizeof(A_B_vectors), cudaMemcpyHostToDevice);
+  cudaMemcpy(v_A_B_buffer, v_A_B_gpu, vector_sizeof(A_B_vectors), cudaMemcpyDeviceToDevice);
+
   cudaMemcpy(v_A_sizes_gpu, A_sizes.data(), vector_sizeof(A_sizes), cudaMemcpyHostToDevice);
 
   unsigned num_threads = min((MAX_BATCH_SIZE / d) * d, N * d);
   unsigned num_blocks = (N * d + num_threads - 1) / num_threads;
-  std::cout << "num_threads: " << num_threads << ", num_blocks: " << num_blocks << std::endl;
 
-  //kernel call
-  emptyk<<<1, 1>>>();
-  TIME_START(timing_kernel);
-  mergeSmallBatch_k<<<num_blocks, num_threads>>>(v_A_B_gpu, v_A_sizes_gpu, N, d);
-  TIME_STOP_SAVE(timing_kernel, timing_kernel_ms);
-
+  float total_time = 0;
+  unsigned int counter = 0;
+  while (total_time < 1)
+  {
+    TIME_START(timing_kernel);
+    mergeSmallBatch_k<<<num_blocks, num_threads>>>(v_A_B_gpu, v_A_sizes_gpu, N, d);
+    TIME_STOP_SAVE(timing_kernel, time_kernel_ms);
+    total_time += time_kernel_ms;
+    
+    if(total_time<1) cudaMemcpy(v_A_B_gpu,v_A_B_buffer, vector_sizeof(A_B_vectors), cudaMemcpyDeviceToDevice);
+    counter++;
+  }
+  time_kernel_ms = total_time /counter;
   cudaMemcpy(vector_out.data(), v_A_B_gpu, vector_sizeof(A_B_vectors), cudaMemcpyDeviceToHost);
 
   float time_cpu = merge_batch_cpu(A_B_vectors, A_sizes, N, d);
-  std::cout << "CPU time: " << timing_kernel_ms << " ms" << std::endl;
-  std::cout << "Kernel time: " << timing_kernel_ms << " ms" << std::endl;
-  std::cout << "Equality: " << (A_B_vectors == vector_out ? "TRUE" : "FALSE") << std::endl;
-
+  bool success = (A_B_vectors == vector_out);
+  std::cout << N << ", " << d << ", " << time_kernel_ms << ", " << time_cpu <<", " << success << std::endl;
   cudaFree(v_A_B_gpu);
   cudaFree(v_A_sizes_gpu);
   TIME_EVENT_DESTROY(timing_kernel);
